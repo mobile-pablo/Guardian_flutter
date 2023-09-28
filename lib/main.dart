efefb343eb8ee8fd9e8fde07ffd021170b274caa
mobile-pablo <pawel.lukasz001@gmail.com>
2023-09-29 01:25:34 +0200

    Migrate to hook widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_drift_1/di/injection_container.dart';
 import 'package:flutter_drift_1/core/app_route.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
@@ -12,23 +13,16 @@ void main() {
 
 @RoutePage()
 @Injectable()
-class MyApp extends StatefulWidget {
-
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends HookWidget {
   final _appRouter = AppRouter();
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp.router(
-        routerDelegate: _appRouter.delegate(),
-        routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
