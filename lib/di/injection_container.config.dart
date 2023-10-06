de21e024b43a8a9ba995b7f6fd0e89e2eb630066
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-06 12:59:11 +0200

    Fix issues

--- lib/di/injection_container.config.dart
+++ lib/di/injection_container.config.dart
@@ -17,6 +17,7 @@ import 'package:flutter_drift_1/feature/home/home_screen.dart' as _i6;
 import 'package:flutter_drift_1/main.dart' as _i8;
 import 'package:flutter_drift_1/networking/service/guardian_service.dart'
     as _i4;
+import 'package:flutter_drift_1/storage/dao/news_dao.dart' as _i11;
 import 'package:flutter_drift_1/storage/dao/news_dao_impl.dart' as _i9;
 import 'package:flutter_drift_1/storage/database/app_database.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
@@ -28,6 +29,7 @@ extension GetItInjectableX on _i1.GetIt {
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
   }) {
+    // ignore: always_specify_types
     final gh = _i2.GetItHelper(
       this,
       environment,
@@ -40,7 +42,7 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i9.NewsDaoImpl>(() => _i9.NewsDaoImpl(gh<_i3.AppDatabase>()));
     gh.factory<_i10.NewsRepositoryImpl>(() => _i10.NewsRepositoryImpl(
           gh<_i4.GuardianService>(),
-          gh<_i9.NewsDaoImpl>(),
+          gh<_i11.NewsDao>(),
         ));
     return this;
   }
