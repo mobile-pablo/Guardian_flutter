058431af696a587b5d8170014a7f4f56d45db42a
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-13 00:12:01 +0200

    Work on ui tests

--- test/widget_tests/home_screen/home_screen_test.dart
+++ test/widget_tests/home_screen/home_screen_test.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:guardian_flutter/di/injection_container.dart';
@@ -7,14 +8,11 @@ import 'package:mockito/annotations.dart';
 
 import 'home_screen.mocks.dart';
 
-
-void setupDependencyInjection() {
-  GetIt.instance.registerSingleton<HomeScreen>(const HomeScreen());
-}
+final GetIt sl = GetIt.instance;
 
 @GenerateMocks(<Type>[StackRouter])
 void main() {
-  setupDependencyInjection();
+  configureInjection();
   TestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Navigate to Initial Screen, Title is Displayed',
@@ -23,11 +21,13 @@ void main() {
       StackRouterScope(
         controller: MockStackRouter(),
         stateHash: 0,
-        child: getIt<HomeScreen>(),
+        child: HomeScreen(
+          key: UniqueKey(),
+        ),
       ),
     );
 
     Future<dynamic>.delayed(const Duration(seconds: 20));
-    expect(find.text('Guardian News'), findsOneWidget);
+    expect(find.text('Home Screen'), findsOneWidget);
   });
 }
