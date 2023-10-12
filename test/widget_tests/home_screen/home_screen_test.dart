eee9a394911376c72be6885900a3e80d63d03d56
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-12 22:26:56 +0200

    Migrate tests to home folder

--- test/widget_tests/home_screen/home_screen_test.dart
+++ test/widget_tests/home_screen/home_screen_test.dart
@@ -1,22 +1,32 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:guardian_flutter/di/injection_container.dart';
 import 'package:guardian_flutter/feature/home/page/home_screen.dart';
 import 'package:mockito/annotations.dart';
 
-import 'app_test.mocks.dart';
+import 'home_screen.mocks.dart';
+
+
+void setupDependencyInjection() {
+  GetIt.instance.registerSingleton<HomeScreen>(const HomeScreen());
+}
 
 @GenerateMocks(<Type>[StackRouter])
 void main() {
+  setupDependencyInjection();
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Navigate to Initial Screen', (WidgetTester tester) async {
+  testWidgets('Navigate to Initial Screen, Title is Displayed',
+      (WidgetTester tester) async {
     await tester.pumpWidget(
       StackRouterScope(
         controller: MockStackRouter(),
         stateHash: 0,
-        child: const HomeScreen(),
+        child: getIt<HomeScreen>(),
       ),
     );
+
     Future<dynamic>.delayed(const Duration(seconds: 20));
     expect(find.text('Guardian News'), findsOneWidget);
   });
