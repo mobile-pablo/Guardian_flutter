0a71f90865b64d7e4f11548812f8fea67aef57be
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-15 17:24:28 +0200

    Work on networking mocks

--- test/mocks/lib/networking/guardian_service_mock.dart
+++ test/mocks/lib/networking/guardian_service_mock.dart
@@ -1,12 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:guardian_flutter/domain/model/news_item/news_item.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/dio.dart';
 
 @Injectable(env: <String>[Environment.test])
-abstract class GuardianServiceMock {
+class GuardianServiceMock {
+  JsonDecoder decoder = const JsonDecoder();
+  String get guardianSearchJson =>
+      File('test/mocks/jsons/guardian_search.json').readAsStringSync();
 
   Future<HttpResponse<List<NewsItem>>> getNews({
-   required String query,
+    required String query,
     required String showFields,
-  });
+  }) async {
+    final Map<String, dynamic> jsonmap = decoder.convert(guardianSearchJson);
+    List<NewsItem> newsItems = await jsonmap['response']['results']
+        .map<NewsItem>(
+          (dynamic i) => NewsItem.fromJson(i as Map<String, dynamic>),
+        )
+        .toList();
+
+    return HttpResponse<List<NewsItem>>(
+      newsItems,
+      Response<List<NewsItem>>(
+        requestOptions: RequestOptions(path: 'test'),
+        data: newsItems,
+        statusCode: HttpStatus.ok,
+      ),
+    );
+  }
 }
