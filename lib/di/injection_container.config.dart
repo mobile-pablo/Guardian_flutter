41cd2db6a671e28583b400e41e5ac0fd6533fd84
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-15 11:04:11 +0200

    Build injection for enviroments

--- lib/di/injection_container.config.dart
+++ lib/di/injection_container.config.dart
@@ -40,44 +40,119 @@ import 'package:guardian_flutter/storage/dao/news_dao_impl.dart' as _i12;
 import 'package:guardian_flutter/storage/database/app_database.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
 
+const String _prod = 'prod';
+const String _dev = 'dev';
+const String _test = 'test';
+
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
   _i1.GetIt init({
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
   }) {
-    final _i2.GetItHelper gh = _i2.GetItHelper(
+    final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
-    final _$AppModule appModule = _$AppModule();
-    gh.factory<_i3.AppDatabase>(() => _i3.AppDatabase());
-    gh.factory<_i4.AppRouter>(() => _i4.AppRouter());
-    gh.factory<_i5.Dio>(() => appModule.dio);
-    gh.factory<_i6.GuardianService>(() => _i6.GuardianService(gh<_i5.Dio>()));
-    gh.factory<_i7.HomeScreen>(() => _i7.HomeScreen(key: gh<_i8.Key>()));
-    gh.factory<_i9.MyApp>(() => _i9.MyApp(key: gh<_i10.Key>()));
-    gh.factory<_i11.NewsDao>(() => _i12.NewsDaoImpl(gh<_i3.AppDatabase>()));
-    gh.factory<_i13.NewsRepository>(() => _i14.NewsRepositoryImpl(
-          gh<_i6.GuardianService>(),
-          gh<_i11.NewsDao>(),
-        ));
+    final appModule = _$AppModule();
+    gh.factory<_i3.AppDatabase>(
+      () => _i3.AppDatabase(),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
+    gh.factory<_i4.AppRouter>(
+      () => _i4.AppRouter(),
+      registerFor: {
+        _prod,
+        _dev,
+        _test,
+      },
+    );
+    gh.factory<_i5.Dio>(
+      () => appModule.dio,
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
+    gh.factory<_i6.GuardianService>(
+      () => _i6.GuardianService(gh<_i5.Dio>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
+    gh.factory<_i7.HomeScreen>(
+      () => _i7.HomeScreen(key: gh<_i8.Key>()),
+      registerFor: {
+        _prod,
+        _dev,
+        _test,
+      },
+    );
+    gh.factory<_i9.MyApp>(
+      () => _i9.MyApp(key: gh<_i10.Key>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
+    gh.factory<_i11.NewsDao>(
+      () => _i12.NewsDaoImpl(gh<_i3.AppDatabase>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
+    gh.factory<_i13.NewsRepository>(
+      () => _i14.NewsRepositoryImpl(
+        gh<_i6.GuardianService>(),
+        gh<_i11.NewsDao>(),
+      ),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
     gh.factory<_i15.RemoveNewsItemUseCase>(
-        () => _i15.RemoveNewsItemUseCase(gh<_i13.NewsRepository>()));
+      () => _i15.RemoveNewsItemUseCase(gh<_i13.NewsRepository>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
     gh.factory<_i16.UpdateNewsItemUseCase>(
         () => _i16.UpdateNewsItemUseCase(gh<_i13.NewsRepository>()));
     gh.factory<_i17.GetNewsUseCase>(
-        () => _i17.GetNewsUseCase(gh<_i13.NewsRepository>()));
+      () => _i17.GetNewsUseCase(gh<_i13.NewsRepository>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
     gh.factory<_i18.HomeRemoteBloc>(
-        () => _i18.HomeRemoteBloc(gh<_i17.GetNewsUseCase>()));
+      () => _i18.HomeRemoteBloc(gh<_i17.GetNewsUseCase>()),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
     gh.factory<_i19.InsertNewsItemUseCase>(
         () => _i19.InsertNewsItemUseCase(gh<_i13.NewsRepository>()));
-    gh.factory<_i20.HomeLocalBloc>(() => _i20.HomeLocalBloc(
-          gh<_i19.InsertNewsItemUseCase>(),
-          gh<_i15.RemoveNewsItemUseCase>(),
-          gh<_i16.UpdateNewsItemUseCase>(),
-        ));
+    gh.factory<_i20.HomeLocalBloc>(
+      () => _i20.HomeLocalBloc(
+        gh<_i19.InsertNewsItemUseCase>(),
+        gh<_i15.RemoveNewsItemUseCase>(),
+        gh<_i16.UpdateNewsItemUseCase>(),
+      ),
+      registerFor: {
+        _prod,
+        _dev,
+      },
+    );
     return this;
   }
 }
