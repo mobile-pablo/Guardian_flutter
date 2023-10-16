2c70ae2ad95b46a6ed8d1ecb0c223623ffc5e07f
mobile-pablo <pawel.lukasz001@gmail.com>
2023-10-16 18:06:30 +0200

    Fix widget test

--- test/widget_tests/home_screen/home_screen_test.dart
+++ test/widget_tests/home_screen/home_screen_test.dart
@@ -1,19 +1,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:guardian_flutter/di/injection_container.dart';
+import 'package:get_it/get_it.dart';
+import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_bloc.dart';
 import 'package:guardian_flutter/feature/home/page/home_screen.dart';
-import 'package:guardian_flutter/networking/service/guardian_service.dart';
 import 'package:mockito/annotations.dart';
 
 import 'home_screen_test.mocks.dart';
 
-@GenerateMocks(<Type>[StackRouter, GuardianService])
+final GetIt sl = GetIt.instance;
+final GetIt getIt = GetIt.I;
 
+@GenerateMocks(<Type>[StackRouter])
 void main() {
-  configureInjection();
   TestWidgetsFlutterBinding.ensureInitialized();
-
+   sl.registerSingleton<HomeRemoteBloc>(sl());
   late StackRouter mockStackRouter;
   setUpAll(() {
     mockStackRouter = MockStackRouter();
@@ -21,15 +22,13 @@ void main() {
 
   testWidgets('Navigate to Initial Screen, Title is Displayed',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      StackRouterScope(
-        controller: mockStackRouter,
-        stateHash: 0,
-        child: HomeScreen(
-          key: UniqueKey(),
-        ),  
-      )
-    );
+    await tester.pumpWidget(StackRouterScope(
+      controller: mockStackRouter,
+      stateHash: 0,
+      child: HomeScreen(
+        key: UniqueKey(),
+      ),
+    ));
 
     expect(find.text('Home Screen'), findsOneWidget);
   });
