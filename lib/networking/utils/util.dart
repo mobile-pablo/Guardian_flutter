53f7e051438ded71963174ab44008dc634b7645e
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-10-20 23:41:06 +0200

    Add test

---
+++ lib/networking/utils/util.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:guardian_flutter/domain/model/news_item/news_item.dart';
+
+Future<List<NewsItem>> getGuardianMockNewsItems() async {
+  JsonDecoder decoder = const JsonDecoder();
+
+  final String guardianSearchJson =
+      await rootBundle.loadString('assets/json/guardian_search.json');
+  final Map<String, dynamic> jsonmap = decoder.convert(guardianSearchJson);
+  List<NewsItem> newsItems = await jsonmap['response']['results']
+      .map<NewsItem>(
+        (dynamic i) => NewsItem.fromJson(i as Map<String, dynamic>),
+      )
+      .toList();
+  return newsItems;
+}
--- test/widget_tests/home/home_widget_test.dart
+++ test/widget_tests/home/home_widget_test.dart
@@ -1,20 +1,36 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:guardian_flutter/core/models/news_item_dto.dart';
 import 'package:guardian_flutter/di/injection_container.dart';
+import 'package:guardian_flutter/domain/mapper/news_dto_mapper.dart';
+import 'package:guardian_flutter/domain/model/news_item/news_item.dart';
+import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_bloc.dart';
+import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_event.dart';
+import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_state.dart';
 import 'package:guardian_flutter/feature/home/page/home_screen.dart';
+import 'package:guardian_flutter/networking/utils/util.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mocktail/mocktail.dart';
 import 'home_widget_test.mocks.dart';
+
+class MockHomeRemoteBloc extends MockBloc<HomeRemoteEvent, HomeRemoteState>
+    implements HomeRemoteBloc {}
 
 @GenerateMocks(<Type>[StackRouter])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   late StackRouter mockStackRouter;
-
+  late MockHomeRemoteBloc mockHomeRemoteBloc;
+  final NewsDTOMapper _newsDTOMapper = NewsDTOMapper();
   setUpAll(() {
     configureInjectionTest();
+    mockHomeRemoteBloc = MockHomeRemoteBloc();
     mockStackRouter = MockStackRouter();
   });
 
@@ -47,6 +63,33 @@ void main() {
             matching: find.byType(CupertinoActivityIndicator),
           ),
           findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Home Screen, Loaded screen is displayed',
+    (WidgetTester tester) async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      List<NewsItem> newsItems = await getGuardianMockNewsItems();
+      List<NewsItemDTO> newsItemsDTO = newsItems
+          .map((NewsItem e) => _newsDTOMapper.convert<NewsItem, NewsItemDTO>(e))
+          .toList();
+
+      when(() => mockHomeRemoteBloc.state)
+          .thenReturn(HomeRemoteNewsDone(newsItemsDTO));
+
+      await tester.pumpWidget(
+        BlocProvider<MockHomeRemoteBloc>.value(
+          value: mockHomeRemoteBloc,
+          child: MaterialApp(
+            home: HomeScreen(
+              key: UniqueKey(),
+            ),
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(find.textContaining('Harry Potta'), findsOneWidget);
     },
   );
 }
