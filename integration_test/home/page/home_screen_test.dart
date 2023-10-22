b24a51f75ba74da20bb9ac816fc4895e4c290f01
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-10-22 22:55:26 +0200

    Fix issues

--- integration_test/home/page/home_screen_test.dart
+++ integration_test/home/page/home_screen_test.dart
@@ -12,6 +12,8 @@ import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_bloc.dart'
 import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_event.dart';
 import 'package:guardian_flutter/feature/home/bloc/remote/home_remote_state.dart';
 import 'package:guardian_flutter/feature/home/page/home_screen.dart';
+import 'package:guardian_flutter/feature/home/widgets/news_item_widget.dart';
+import 'package:guardian_flutter/feature/home/wrapper/news_item_wrapper.dart';
 import 'package:guardian_flutter/networking/utils/util.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -86,4 +88,42 @@ void main() {
       expect(find.textContaining('Harry Potta'), findsOneWidget);
     },
   );
+
+  testWidgets('News Item Widget, View displayed', (WidgetTester tester) async {
+    NewsItemWrapper wrapper = const NewsItemWrapper(
+      'Harry Potta and the £4.50 chocolate frog | Alice O Keeffe',
+      'assets/images/default_image.jpg',
+      'I was delighted when my son discovered JK Rowling s books',
+      'https://www.theguardian.com/harry-potter-magic-jk-rowling',
+    );
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Home Screen',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          body: Row(
+            children: [
+              NewsItemWidget(wrapper: wrapper),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    final Finder newsItemTitle = find.text(wrapper.title);
+    final Finder newsItemDescription = find.text(wrapper.description);
+    final Finder newsItemImage = find.image(AssetImage(wrapper.imageUrl));
+
+    expect(newsItemTitle, findsOneWidget);
+    expect(newsItemDescription, findsOneWidget);
+    expect(newsItemImage, findsOneWidget);
+  });
 }
