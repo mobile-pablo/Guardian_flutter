d5345a862e715762c4c3be91a4f178468bc58bcd
Paweł Łukasz <pawel.lukasz001@gmail.com>
2023-10-23 21:39:25 +0200

    Fix mocks via GenerateNiceMocks

--- test/unit_tests/networking/guardian_service_test.dart
+++ test/unit_tests/networking/guardian_service_test.dart
@@ -1,18 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:guardian_flutter/domain/model/news_item/news_item.dart';
 import 'package:guardian_flutter/networking/service/guardian_service.dart';
+import 'package:guardian_flutter/networking/utils/util.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
 
-import 'guardian_service_test.dart';
+import 'guardian_service_test.mocks.dart';
 
-@GenerateMocks([GuardianService])
+@GenerateNiceMocks([MockSpec<GuardianService>()])
 void main() {
-  GuardianService mockGuardianService;
+  late GuardianService mockGuardianService;
   setUpAll(() {
     mockGuardianService = MockGuardianService();
   });
 
   test('Get news', () async {
-    await mockGuardianService.getNews(
+    dynamic getNewsCall = mockGuardianService.getNews(
         query: "Dogs", showFields: "thumbnail,bodyText");
+
+    when(getNewsCall).thenAnswer(
+      (_) async {
+        List<NewsItem> newsItems = await getGuardianMockNewsItems();
+        return HttpResponse<List<NewsItem>>(
+          newsItems,
+          Response<List<NewsItem>>(
+            requestOptions: RequestOptions(path: 'test'),
+            data: newsItems,
+            statusCode: HttpStatus.ok,
+          ),
+        );
+      },
+    );
+
+    expect(getNewsCall, isA<HttpResponse<List<NewsItem>>>());
   });
 }
